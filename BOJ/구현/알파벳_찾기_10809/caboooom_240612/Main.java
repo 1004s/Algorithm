6d48f9bc3911b045d2093308d43eeee2efcf39c0
sylim <sammys27@naver.com>
2024-06-12 16:32:36 +0900

    add Main

--- BOJ/구현/알파벳_찾기_10809/caboooom_240612/Main.java
+++ BOJ/구현/알파벳_찾기_10809/caboooom_240612/Main.java
@@ -1,4 +1,7 @@
 package BOJ.구현.알파벳_찾기_10809.caboooom_240612;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ BOJ/구현/알파벳_찾기_10809/insub2004_240612/Main.java
@@ -0,0 +1,4 @@
+package BOJ.구현.알파벳_찾기_10809.insub2004_240612;
+
+public class Main {
+}
