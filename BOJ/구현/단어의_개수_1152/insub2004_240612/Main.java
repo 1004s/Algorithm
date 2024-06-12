79884e32a2b1407d71a11495ce2bee70053b22ef
insub2004 <insub2004@naver.com>
2024-06-12 16:59:40 +0900

    [01] 1152. 단어의 개수 / 브론즈2 / 15분 / 실패
    https://www.acmicpc.net/problem/1152

--- BOJ/구현/단어의_개수_1152/insub2004_240612/Main.java
+++ BOJ/구현/단어의_개수_1152/insub2004_240612/Main.java
@@ -1,4 +1,16 @@
 package BOJ.구현.단어의_개수_1152.insub2004_240612;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().trim().split(" ");
+
+        System.out.println(str.length);
+    }
 }
--- BOJ/구현/알파벳_찾기_10809/insub2004_240612/Main.java
+++ BOJ/구현/알파벳_찾기_10809/insub2004_240612/Main.java
@@ -1,4 +1,5 @@
 package BOJ.구현.알파벳_찾기_10809.insub2004_240612;
 
 public class Main {
+
 }
