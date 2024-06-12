9651e271c2918315b37b13129d3ea53f9712045b
sylim <sammys27@naver.com>
2024-06-12 17:03:31 +0900

    [01] 1152. 단어의 개수 / 브론즈2 / 15분 / 실패
    https://www.acmicpc.net/problem/1152
    왜 안 됨?

--- BOJ/구현/단어의_개수_1152/caboooom_240612/Main.java
+++ BOJ/구현/단어의_개수_1152/caboooom_240612/Main.java
@@ -1,4 +1,12 @@
 package BOJ.구현.단어의_개수_1152.caboooom_240612;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strLine = reader.readLine().trim().split(" ");
+        System.out.println(strLine.length);
+    }
 }
