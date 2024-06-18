5e396734443cf3f44957f46104eb79d422c7b7a1
insub2004 <insub2004@naver.com>
2024-06-18 22:59:25 +0900

    [02] 2204. 도비의 난독증 테스트 / 브론즈1 / 20분 / 성공
    https://www.acmicpc.net/problem/2204
    
    조금 도움을 받음...

--- BOJ/구현/도비의_난독증_테스트_2204/insub2004_240618/Main.java
+++ BOJ/구현/도비의_난독증_테스트_2204/insub2004_240618/Main.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -30,12 +27,20 @@ public class Main {
             for (int i = 1; i < list.size(); i++) {
                 String target = list.get(i).toLowerCase();
 
-                for(int j = 0; j < min.length(); j++) {
+                for (int j = 0; j < min.length(); j++) {
                     if (min.charAt(j) > target.charAt(j)) {
                         min = target.toLowerCase();
                         result = i;
                         break;
+                    } else if (min.charAt(j) == target.charAt(j)) {
+                        if (j == target.length()-1) {
+                            min = target.toLowerCase();
+                            result = i;
+                            break;
+                        }
+                        continue;
                     }
+                    break;
                 }
             }
 
