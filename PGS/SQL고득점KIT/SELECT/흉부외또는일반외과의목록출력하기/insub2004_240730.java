adc11dc18d1f1d7d3f5bf5b9d6a5ed58ae6d338e
insub2004 <insub2004@naver.com>
2024-07-30 09:55:18 +0900

    [01] 흉부외또는일반외과의목록출력하기 / SELECT / 5분 / 1차제출
    https://school.programmers.co.kr/learn/courses/30/lessons/132203

---
+++ PGS/SQL고득점KIT/SELECT/흉부외또는일반외과의목록출력하기/insub2004_240730.java
@@ -0,0 +1,11 @@
+package PGS.SQL고득점KIT.SELECT.흉부외또는일반외과의목록출력하기;
+
+public class insub2004_240730 {
+    /*
+    -- 코드를 입력하세요
+    SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d')
+    FROM DOCTOR
+    WHERE MCDP_CD IN ("CS", "GS")
+    ORDER BY HIRE_YMD DESC, DR_NAME ASC
+     */
+}
