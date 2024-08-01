69e727bedbc7d9f7ac597095540e1e23b08a7231
insub2004 <insub2004@naver.com>
2024-08-01 09:03:03 +0900

    [01] rny_string / Lv0 / 10초 / 1차제출
    https://school.programmers.co.kr/learn/courses/30/lessons/181856

---
+++ PGS/level_0/rny_string/insub2004_240801.java
@@ -0,0 +1,13 @@
+package PGS.level_0.rny_string;
+
+public class insub2004_240801 {
+
+    public static String solution(String rny_string) {
+        String answer = rny_string;
+
+        return answer.replaceAll("m","rn");
+    }
+    public static void main(String[] args) {
+
+    }
+}
