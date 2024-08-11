75a4bbf71a0fd92ab718bf833dbbc1da5444ad0b
sylim <sammys27@naver.com>
2024-08-12 00:14:40 +0900

    [01] 1158. 요세푸스 문제 / 20분 / 성공 https://www.acmicpc.net/problem/1158 출력조건..

---
+++ BOJ/자료구조/요세푸스_문제_1158/caboooom_240811/Main.java
@@ -0,0 +1,32 @@
+package BOJ.자료구조.요세푸스_문제_1158.caboooom_240811;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = reader.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+
+        List<Integer> list = new LinkedList<>();
+        for(int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+
+        int target = k - 1;
+        StringBuilder builder = new StringBuilder("<");
+        while(!list.isEmpty()) {
+            target = target % list.size();
+            int data = list.remove(target);
+            builder.append(data + ", ");
+            target += (k-1);
+        }
+        builder.delete(builder.length()-2, builder.length()).append(">");
+        System.out.println(builder);
+    }
+}
