b5a11363bf35ad040463947dc980609d43f93349
sylim <sammys27@naver.com>
2024-08-14 09:10:46 +0900

    [02] 1914. 하노이 탑 / 11분 / 성공 https://www.acmicpc.net/problem/1914 출력 조건 파악과 큰 수 처리

--- BOJ/재귀/하노이_탑_1914/caboooom_240814/Main.java
+++ BOJ/재귀/하노이_탑_1914/caboooom_240814/Main.java
@@ -3,14 +3,17 @@ package BOJ.재귀.하노이_탑_1914.caboooom_240814;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(reader.readLine());
-        int k = (int) Math.pow(2, n) - 1;
-        System.out.println(k);
-        hanoiTower(n, 1, 2, 3);
+        BigInteger n = new BigInteger(reader.readLine());
+        System.out.println((new BigInteger("2").pow(n.intValue()))
+                .subtract(new BigInteger("1")));
+        if(n.intValue() <= 20) {
+            hanoiTower(n.intValue(), 1, 2, 3);
+        }
     }
 
     public static void hanoiTower(int n, int origin, int temp, int dest) {
@@ -19,9 +22,7 @@ public class Main {
             return;
         }
         hanoiTower(n-1, origin, dest, temp);
-        if(n <= 20) {
-            System.out.println(origin + " " + dest);
-        }
+        System.out.println(origin + " " + dest);
         hanoiTower(n-1, temp, origin, dest);
     }
 }
